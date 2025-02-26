b31608fafe648ec0308b8b8c101c4f18ab8dc5d9
wbdevmaguranye <jmaguranye@gmail.com>
2025-02-26 11:50:01 +0000

    Added actions for adding, removing, and updating todos

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -3,8 +3,22 @@ import { defineStore } from 'pinia';
 export const useTodoStore = defineStore('todo', {
   state: () => ({
     todos: [],
-   
-    
   }),
+  actions :{
+    addTodo(text){
+        this.todos.push({id: Date.now(), text, completed:false});
+    },
+    removeTodo(id){
+        this.todos = this.todos.filter(todo =>todo.id !== id);
+    },
+    toggleTodo(id){
+        const todo = this.todos.find(todo =>todo.id === id);
+        if (todo) todo.completed = !todo.completed;
+    },
+    updateTodo(id,newText){
+        const todo = this.todos.find(todo => todo.id === id);
+        if (todo) todo.text = newText;
+    }
+  }
 
 });
